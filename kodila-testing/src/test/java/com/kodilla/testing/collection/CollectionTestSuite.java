f85fe7cc0205d736761ae5d1a8cf5667c48573f6
Mateusz <mszczerba86@gmail.com>
2021-04-07 21:57:00 +0200

    .

--- kodila-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodila-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,7 +1,6 @@
 package com.kodilla.testing.collection;
 
 import org.junit.jupiter.api.*;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -12,29 +11,24 @@ public class CollectionTestSuite {
     public static void beforeAll() {
         System.out.println("Test Suite: begin");
     }
-
     @AfterAll
     public static void afterAll() {
         System.out.println("Test Suite: end");
     }
-
     @BeforeEach
     public void before() {
         System.out.println("Test Case: begin");
     }
-
     @AfterEach
     public void after() {
         System.out.println("Test Case: end");
     }
-
     @DisplayName("When create oddNumberExterminator "+"then exterminate should return empty list on list without any numbers"
     )
     @Test
     void testOddNumbersExterminatorEmptyList() {
         // Given
         ArrayList<Integer> emptyList=new ArrayList<>();
-
         OddNumbersExterminator oddNumbersExterminator=new OddNumbersExterminator();
         // When
         List<Integer> oddNumbers=oddNumbersExterminator.exterminate(emptyList);
@@ -50,12 +44,12 @@ public class CollectionTestSuite {
     @Test
     void testOddNumbersExterminatorNormalList() {
         // Given
-        ArrayList<Integer> numberList =new ArrayList<>(Arrays.asList(1,3,5,13,22,24,));
+        ArrayList<Integer> numberList =new ArrayList<>(Arrays.asList (1,3,5,13,22,24));
         OddNumbersExterminator oddNumbersExterminator =new OddNumbersExterminator();
         // When
         List<Integer> oddNumbers=oddNumbersExterminator.exterminate(numberList);
         List<Integer> expectedResult=Arrays.asList(6,12,18,24);
-        System.out.println("Testing if number list: " +oddNumbers+ " matches numbers: " + expectedResult);
+        System.out.println("Testing if number list: " + oddNumbers + " matches numbers: " + expectedResult);
         Collections.sort(oddNumbers);
         Collections.sort(expectedResult);
         // Then
