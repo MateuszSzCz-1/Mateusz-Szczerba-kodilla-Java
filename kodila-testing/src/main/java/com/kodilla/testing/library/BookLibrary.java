3c6c627a71eb640f08bb719b5feb4aaeaa085305
Mateusz <mszczerba86@gmail.com>
2021-04-14 17:55:29 +0200

    6.6 Enlargement BookLibrary

--- kodila-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodila-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -1,7 +1,9 @@
 package com.kodilla.testing.library;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BookLibrary {
     LibraryDatabase libraryDatabase;
@@ -9,7 +11,6 @@ public class BookLibrary {
     public BookLibrary(LibraryDatabase libraryDatabase) {
         this.libraryDatabase = libraryDatabase;
     }
-
     public List<Book> listBooksWithCondition(String titleFragment) {
         List<Book> bookList = new ArrayList<Book>();
         if (titleFragment.length() < 3) return bookList;
@@ -18,5 +19,32 @@ public class BookLibrary {
         if (resultList.size() > 20) return bookList;
         bookList = resultList;
         return bookList;
+    }
+    public List<Book> generateBookList() {
+        List<Book> bookList = new ArrayList<>();
+        Book book1 = new Book("Secrets of Alamo", "John Smith", 2008);
+        Book book2 = new Book("Secretaries and Directors", "Dilbert Michigan", 2012);
+        Book book3 = new Book("Secret life of programmers", "Steve Wolkowitz", 2016);
+        Book book4 = new Book("Secrets of Java", "Ian Tenewitch", 2010);
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        bookList.add(book4);
+
+        return bookList;
+    }
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
+        List<Book> rentedBookList = new ArrayList<>();
+        Map<LibraryUser, Book> libraryUsers = new HashMap<>();
+        List<Book> resultList = libraryDatabase
+                .listBooksInHandsOf(libraryUser);
+
+
+        for (Map.Entry entry : libraryUsers.entrySet()) {
+            resultList.add(libraryUsers.get(entry));
+        }
+
+        return resultList;
     }
 }
--- kodila-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
+++ kodila-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
@@ -5,11 +5,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -37,7 +35,7 @@ class BookDirectoryTestSuite {
         List<Book> theListOfBooks = bookLibrary.listBooksWithCondition("Secret");
 
         // Then
-        assertEquals(4, theListOfBooks.size());;
+        assertEquals(4, theListOfBooks.size());
     }
     private List<Book> generateListOfNBooks(int booksQuantity) {
         List<Book> resultList = new ArrayList<>();
@@ -83,5 +81,68 @@ class BookDirectoryTestSuite {
         // Then
         assertEquals(0, theListOfBooks10.size());
         verify(libraryDatabaseMock, times(0)).listBooksWithCondition(anyString());
+    }
+    @Test
+    void testListBooksInHandsOfWithConditionEmptyResultList() {
+        //Given
+        BookLibrary bookLibrary = new BookLibrary(libraryDatabaseMock);
+        LibraryUser libraryUser = new LibraryUser("Józek", "Grażyna", "741206754");
+        Map<Book, LibraryUser> resultListOfZeroRentedBook = new HashMap<>();
+        Set<Book> keySet = resultListOfZeroRentedBook.keySet();
+        List<Book> resultListOfBooks = new ArrayList<>(keySet);
+        when(libraryDatabaseMock.listBooksInHandsOf(libraryUser)).thenReturn(resultListOfBooks);
+        //When
+        List<Book> theListOfBooks0 = bookLibrary.listBooksInHandsOf(libraryUser);
+        //Then
+        assertEquals(0, theListOfBooks0.size());
+    }
+
+    @Test
+    void testListBooksInHandsOfWithConditionExactlyOneBook() {
+        //Given
+        BookLibrary bookLibrary = new BookLibrary(libraryDatabaseMock);
+        LibraryUser libraryUser = new LibraryUser("Józek", "Grażyna", "741206754");
+        Book book4 = new Book("Book1", "Autor 1", 2019);
+        Map<Book, LibraryUser> resultListOfOneRentedBook = new HashMap<>();
+        resultListOfOneRentedBook.put(book4, libraryUser);
+        Set<Book> keySet = resultListOfOneRentedBook.keySet();
+        List<Book> resultListOfBooks = new ArrayList<>(keySet);
+        when(libraryDatabaseMock.listBooksInHandsOf(libraryUser)).thenReturn(resultListOfBooks);
+
+        //When
+        List<Book> theListOfBooks1 = bookLibrary.listBooksInHandsOf(libraryUser);
+
+        //Then
+        assertEquals(1, theListOfBooks1.size());
+    }
+
+    @Test
+    void testListBooksInHandsOfWithConditionExactlyFiveBooks() {
+        //Given
+        BookLibrary bookLibrary = new BookLibrary(libraryDatabaseMock);
+        LibraryUser libraryUser = new LibraryUser("Józek", "Grażyna", "741206754");
+        Book book1 = new Book("book1", "author1", 2019);
+        Book book2 = new Book("book2", "author2", 2018);
+        Book book3 = new Book("book3", "author3", 2017);
+        Book book4 = new Book("book4", "author4", 2016);
+        Book book5 = new Book("book5", "author5", 2001);
+        Map<Book, LibraryUser> resultListOfFiveRentedBook = new HashMap<>();
+        resultListOfFiveRentedBook.put(book1, libraryUser);
+        resultListOfFiveRentedBook.put(book2, libraryUser);
+        resultListOfFiveRentedBook.put(book3, libraryUser);
+        resultListOfFiveRentedBook.put(book4, libraryUser);
+        resultListOfFiveRentedBook.put(book5, libraryUser);
+
+        Set<Book> keySet = resultListOfFiveRentedBook.keySet();
+
+        List<Book> resultListOfBooks = new ArrayList<>(keySet);
+
+        when(libraryDatabaseMock.listBooksInHandsOf(libraryUser)).thenReturn(resultListOfBooks);
+
+        //When
+        List<Book> theListOfBooks5 = bookLibrary.listBooksInHandsOf(libraryUser);
+
+        //Then
+        assertEquals(5, theListOfBooks5.size());
     }
 }
