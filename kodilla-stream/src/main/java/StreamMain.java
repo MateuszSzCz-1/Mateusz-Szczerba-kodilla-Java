da9162a28299a0117faab705406a5ccc5b1a9d96
Mateusz <mszczerba86@gmail.com>
2021-04-15 12:41:39 +0200

    7.3

--- kodilla-stream/src/main/java/StreamMain.java
+++ kodilla-stream/src/main/java/StreamMain.java
@@ -1,9 +1,16 @@
 
 import com.kodilla.stream.beautification.Deco;
 import com.kodilla.stream.beautification.PoemBeautifier;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class StreamMain {
@@ -30,7 +37,14 @@ public class StreamMain {
         // poemBeautifier.beautify("a kot ma", Deco::decoWithHash);
 
 
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        //System.out.println("Using Stream to generate even numbers from 1 to 20");
+        //NumbersGenerator.generateEven(20);
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()  // [1]
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n", "<<", ">>"));                    // [2]
+
+        System.out.println(theResultStringOfBooks);
     }
 }
