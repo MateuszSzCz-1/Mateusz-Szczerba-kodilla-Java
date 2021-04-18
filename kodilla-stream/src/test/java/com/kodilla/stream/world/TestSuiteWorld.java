a449a81ecd996b5ab793ddbbe2430b7ad1ba532f
Mateusz <mszczerba86@gmail.com>
2021-04-18 16:53:43 +0200

    7.4 We calculate the number of people in the world.

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/TestSuiteWorld.java
@@ -0,0 +1,29 @@
+package com.kodilla.stream.world;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSuiteWorld {
+    @Test
+    void testGetPeopleQuantity() {
+        //Given
+        Country taiwan = new Country("Taiwan", new BigDecimal("10000000"));
+        Country brasil = new Country("Brasil", new BigDecimal("9999"));
+        Continent southAmerica = new Continent("South America");
+        Continent asia = new Continent("Asia");
+        southAmerica.addCountry(brasil);
+        asia.addCountry(taiwan);
+        World world = new World();
+        world.addContinent(southAmerica);
+        world.addContinent(asia);
+
+        //When
+        BigDecimal globalPopulation = world.getPeopleQuantity();
+
+        //Then
+        assertEquals(new BigDecimal("10009999"), globalPopulation);
+    }
+}
